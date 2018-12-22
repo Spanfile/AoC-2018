13aa48e7d6af498bdbbb6d2c9ba7ce7a2d1df56d
Spanfile <spansdev@gmail.com>
2018-12-22 12:09:39 +0200

    ok it doesn't underflow now

--- src/puzzles/day15.rs
+++ src/puzzles/day15.rs
@@ -78,7 +78,7 @@ impl Unit {
 
     fn turn(&mut self, cave: &Cave) -> bool {
         if !self.is_alive() {
-            println!("{:?} {} is dead", self.unit_type, self.id);
+            // println!("{:?} {} is dead", self.unit_type, self.id);
             return false;
         }
 
@@ -95,7 +95,7 @@ impl Unit {
 
         let squares_in_range = self.squares_in_range(cave, &possible_targets);
         let self_index = cave.map.coords_to_index(self.x, self.y);
-        let mut closest = (std::f64::MAX, 0);
+        let mut closest = (std::i32::MAX, 0);
 
         for (index, _) in &squares_in_range {
             let distance = cave.map.distance_from_to(self_index, *index);
@@ -122,23 +122,29 @@ impl Unit {
         let mut target = cave.units[&best.1].borrow_mut();
         target.health -= self.attack_power;
 
-        println!(
-            "{:?} {} at {},{} attacks {:?} {} at {},{} leaving them at {} health",
-            self.unit_type,
-            self.id,
-            self.x,
-            self.y,
-            target.unit_type,
-            target.id,
-            target.x,
-            target.y,
-            target.health
-        );
+        // println!(
+        //     "{:?} {} at {},{} attacks {:?} {} at {},{} leaving them at {} health",
+        //     self.unit_type,
+        //     self.id,
+        //     self.x,
+        //     self.y,
+        //     target.unit_type,
+        //     target.id,
+        //     target.x,
+        //     target.y,
+        //     target.health
+        // );
     }
 
     fn move_towards(&mut self, cave: &Cave, point_index: usize) {
         let adjacent = self.get_free_adjacent(cave);
-        let mut best = (std::f64::MAX, 0);
+
+        if adjacent.is_empty() {
+            // println!("{:?} {} cannot move", self.unit_type, self.id);
+            return;
+        }
+
+        let mut best = (std::i32::MAX, 0);
 
         for adj in &adjacent {
             let dist = cave.map.distance_from_to(*adj, point_index);
@@ -153,10 +159,10 @@ impl Unit {
     fn move_to_index(&mut self, cave: &Cave, index: i32) {
         let new_x = index % cave.map.dimension;
         let new_y = (index / cave.map.dimension) % cave.map.dimension;
-        println!(
-            "{:?} {} moves from {},{} to {},{}",
-            self.unit_type, self.id, self.x, self.y, new_x, new_y
-        );
+        // println!(
+        //     "{:?} {} moves from {},{} to {},{}",
+        //     self.unit_type, self.id, self.x, self.y, new_x, new_y
+        // );
         self.x = new_x;
         self.y = new_y;
     }
@@ -306,11 +312,11 @@ impl Map {
         )
     }
 
-    fn distance_from_to(&self, from: usize, to: usize) -> f64 {
+    fn distance_from_to(&self, from: usize, to: usize) -> i32 {
         let from = self.index_to_coords(from);
         let to = self.index_to_coords(to);
 
-        f64::from((to.0 - from.0).pow(2) + (to.1 - from.1).pow(2)).sqrt()
+        f64::from((to.0 - from.0).pow(2) + (to.1 - from.1).pow(2)).sqrt() as i32
     }
 }
 
@@ -418,19 +424,19 @@ fn parse_input(input: Input) -> Cave {
 #[aoc(15)]
 fn solve_1(input: Input) {
     let cave = parse_input(input);
-    cave.print();
+    // cave.print();
 
     let mut turn_counter = 0;
     loop {
         turn_counter += 1;
-        println!("==========\nTurn {}\n==========", turn_counter);
+        // println!("==========\nTurn {}\n==========", turn_counter);
 
         // wtf rust, where is my do-while?
         if !cave.turn() {
             break;
         }
 
-        cave.print();
+        // cave.print();
     }
 
     let health_sum = cave.unit_health_sum();
