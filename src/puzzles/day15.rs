de983f8429dac5450cd029439ce077ca19ab74bf
Spanfile <spansdev@gmail.com>
2018-12-22 11:47:56 +0200

    what'd I say about a lot of work?

--- src/puzzles/day15.rs
+++ src/puzzles/day15.rs
@@ -20,6 +20,13 @@ enum Tile {
     Wall,
 }
 
+#[derive(Debug)]
+enum Occupation {
+    Unit(usize, i32),
+    Free(usize),
+    Wall(usize),
+}
+
 #[derive(Debug, Clone)]
 struct Unit {
     id: i32,
@@ -27,12 +34,20 @@ struct Unit {
     y: i32,
     unit_type: UnitType,
     health: i32,
+    attack_power: i32,
 }
 
 #[derive(Debug)]
 struct Map {
     tiles: Vec<Tile>,
     dimension: i32,
+}
+
+#[derive(Debug)]
+struct Cave {
+    map: Map,
+    units: HashMap<i32, RefCell<Unit>>,
+    unit_max_id: i32,
 }
 
 impl Unit {
@@ -43,6 +58,7 @@ impl Unit {
             y,
             unit_type,
             health: 200,
+            attack_power: 3,
         }
     }
 
@@ -56,39 +72,154 @@ impl Unit {
         );
     }
 
-    fn turn(&mut self, map: &Map, units: &HashMap<i32, RefCell<Unit>>) -> bool {
-        let possible_targets = self.find_possible_targets(units);
+    fn is_alive(&self) -> bool {
+        self.health > 0
+    }
+
+    fn turn(&mut self, cave: &Cave) -> bool {
+        if !self.is_alive() {
+            println!("{:?} {} is dead", self.unit_type, self.id);
+            return false;
+        }
+
+        let possible_targets = self.find_possible_targets(cave);
         if possible_targets.is_empty() {
             return false;
         }
 
-        let squares_in_range = self.squares_in_range(map, &possible_targets, units);
-        let self_index = map.coords_to_index(self.x, self.y);
-        let mut closest = (std::f64::MAX, std::usize::MAX);
+        let adjacent_enemies = self.get_adjacent_enemies(cave);
+        if !adjacent_enemies.is_empty() {
+            self.attack(cave, adjacent_enemies);
+            return true;
+        }
 
-        for (index, target) in &squares_in_range {
-            if self_index == *index {
-                self.attack(*target);
-                return true;
-            } else {
-                let distance = map.distance_from_to(self_index, *index);
-                if distance < closest.0 {
-                    closest = (distance, *index);
-                }
+        let squares_in_range = self.squares_in_range(cave, &possible_targets);
+        let self_index = cave.map.coords_to_index(self.x, self.y);
+        let mut closest = (std::f64::MAX, 0);
+
+        for (index, _) in &squares_in_range {
+            let distance = cave.map.distance_from_to(self_index, *index);
+            if distance < closest.0 {
+                closest = (distance, *index);
             }
         }
 
-        self.move_towards(closest.1);
+        self.move_towards(cave, closest.1);
 
         true
     }
 
-    fn attack(&self, target: i32) {}
+    fn attack(&self, cave: &Cave, enemies: Vec<i32>) {
+        let mut best = (std::i32::MAX, -1);
 
-    fn move_towards(&self, point_index: usize) {}
+        for id in &enemies {
+            let enemy = cave.units[id].borrow();
+            if enemy.health < best.0 {
+                best = (enemy.health, *id);
+            }
+        }
 
-    fn find_possible_targets(&self, units: &HashMap<i32, RefCell<Unit>>) -> Vec<i32> {
-        units
+        let mut target = cave.units[&best.1].borrow_mut();
+        target.health -= self.attack_power;
+
+        println!(
+            "{:?} {} at {},{} attacks {:?} {} at {},{} leaving them at {} health",
+            self.unit_type,
+            self.id,
+            self.x,
+            self.y,
+            target.unit_type,
+            target.id,
+            target.x,
+            target.y,
+            target.health
+        );
+    }
+
+    fn move_towards(&mut self, cave: &Cave, point_index: usize) {
+        let adjacent = self.get_free_adjacent(cave);
+        let mut best = (std::f64::MAX, 0);
+
+        for adj in &adjacent {
+            let dist = cave.map.distance_from_to(*adj, point_index);
+            if dist < best.0 {
+                best = (dist, *adj);
+            }
+        }
+
+        self.move_to_index(cave, best.1 as i32);
+    }
+
+    fn move_to_index(&mut self, cave: &Cave, index: i32) {
+        let new_x = index % cave.map.dimension;
+        let new_y = (index / cave.map.dimension) % cave.map.dimension;
+        println!(
+            "{:?} {} moves from {},{} to {},{}",
+            self.unit_type, self.id, self.x, self.y, new_x, new_y
+        );
+        self.x = new_x;
+        self.y = new_y;
+    }
+
+    fn get_free_adjacent(&self, cave: &Cave) -> Vec<usize> {
+        let mut adjacent = Vec::new();
+
+        // this order is important; it's in reading-order of the tile indices
+        if let Occupation::Free(index) = cave.check_occupation(self.x, self.y - 1) {
+            adjacent.push(index);
+        }
+
+        if let Occupation::Free(index) = cave.check_occupation(self.x - 1, self.y) {
+            adjacent.push(index);
+        }
+
+        if let Occupation::Free(index) = cave.check_occupation(self.x + 1, self.y) {
+            adjacent.push(index);
+        }
+
+        if let Occupation::Free(index) = cave.check_occupation(self.x, self.y + 1) {
+            adjacent.push(index);
+        }
+
+        adjacent
+    }
+
+    fn get_adjacent_enemies(&self, cave: &Cave) -> Vec<i32> {
+        let mut enemies = Vec::new();
+
+        if let Occupation::Unit(_, id) = cave.check_occupation(self.x, self.y - 1) {
+            let enemy = cave.units[&id].borrow();
+            if enemy.unit_type == self.get_opponent_type() && enemy.is_alive() {
+                enemies.push(id);
+            }
+        }
+
+        if let Occupation::Unit(_, id) = cave.check_occupation(self.x - 1, self.y) {
+            let enemy = cave.units[&id].borrow();
+            if enemy.unit_type == self.get_opponent_type() && enemy.is_alive() {
+                enemies.push(id);
+            }
+        }
+
+        if let Occupation::Unit(_, id) = cave.check_occupation(self.x + 1, self.y) {
+            let enemy = cave.units[&id].borrow();
+            if enemy.unit_type == self.get_opponent_type() && enemy.is_alive() {
+                enemies.push(id);
+            }
+        }
+
+        if let Occupation::Unit(_, id) = cave.check_occupation(self.x, self.y + 1) {
+            let enemy = cave.units[&id].borrow();
+            if enemy.unit_type == self.get_opponent_type() && enemy.is_alive() {
+                enemies.push(id);
+            }
+        }
+
+        enemies
+    }
+
+    fn find_possible_targets(&self, cave: &Cave) -> Vec<i32> {
+        cave.units
             .iter()
             .filter_map(|(id, unit)| {
                 if *id != self.id && unit.borrow().unit_type == self.get_opponent_type() {
@@ -107,39 +238,22 @@ impl Unit {
         }
     }
 
-    fn squares_in_range(
-        &self,
-        map: &Map,
-        target_units: &[i32],
-        units: &HashMap<i32, RefCell<Unit>>,
-    ) -> Vec<(usize, i32)> {
+    fn squares_in_range(&self, cave: &Cave, target_units: &[i32]) -> Vec<(usize, i32)> {
         let mut possible_squares = Vec::new();
 
         for target_id in target_units {
-            let target_unit = units[&target_id].borrow();
+            let target_unit = cave.units[&target_id].borrow();
 
             for x_step in (-1..1).step_by(2) {
                 let check_x = target_unit.x + x_step;
-                let (index, tile) = map.get_tile_at_coords(check_x, self.y);
-                if *tile == Tile::Floor {
-                    let unit_blocking = units.iter().find(|(id, unit_cell)| {
-                        if *id == target_id || **id == self.id {
-                            return false;
-                        }
-                        let unit = unit_cell.borrow();
-                        map.coords_to_index(unit.x, unit.y) == index
-                    });
-
-                    if unit_blocking.is_none() {
-                        possible_squares.push((index, *target_id));
-                    }
+                if let Occupation::Free(index) = cave.check_occupation(check_x, self.y) {
+                    possible_squares.push((index, *target_id));
                 }
             }
 
             for y_step in (-1..1).step_by(2) {
                 let check_y = target_unit.y + y_step;
-                let (index, tile) = map.get_tile_at_coords(self.x, check_y);
-                if *tile == Tile::Floor {
+                if let Occupation::Free(index) = cave.check_occupation(self.x, check_y) {
                     possible_squares.push((index, *target_id));
                 }
             }
@@ -200,7 +314,67 @@ impl Map {
     }
 }
 
-fn parse_input(input: Input) -> (HashMap<i32, RefCell<Unit>>, Map, i32) {
+impl Cave {
+    fn new(map: Map, units: HashMap<i32, RefCell<Unit>>, unit_max_id: i32) -> Cave {
+        Cave {
+            map,
+            units,
+            unit_max_id,
+        }
+    }
+
+    fn print(&self) {
+        self.map.print_with_units(&self.units);
+    }
+
+    fn turn(&self) -> bool {
+        let mut id_indices = self
+            .units
+            .iter()
+            .map(|(id, unit_cell)| {
+                let unit = unit_cell.borrow();
+                (*id, self.map.coords_to_index(unit.x, unit.y))
+            })
+            .collect::<Vec<(i32, usize)>>();
+        id_indices.sort_by(|a, b| a.1.cmp(&b.1));
+
+        let mut result = false;
+        for (id, _) in id_indices {
+            let mut unit = self.units[&id].borrow_mut();
+            result |= unit.turn(&self);
+        }
+        result
+    }
+
+    fn check_occupation(&self, x: i32, y: i32) -> Occupation {
+        let (index, tile) = self.map.get_tile_at_coords(x, y);
+        if *tile == Tile::Floor {
+            if let Some(unit_blocking) =
+                self.units
+                    .iter()
+                    .find(|(_, unit_cell)| match unit_cell.try_borrow() {
+                        Ok(unit) => unit.x == x && unit.y == y,
+                        Err(_) => false,
+                    })
+            {
+                return Occupation::Unit(index, *unit_blocking.0);
+            } else {
+                return Occupation::Free(index);
+            }
+        }
+
+        Occupation::Wall(index)
+    }
+
+    fn unit_health_sum(&self) -> i32 {
+        self.units
+            .values()
+            .map(|unit_cell| unit_cell.borrow().health)
+            .sum()
+    }
+}
+
+fn parse_input(input: Input) -> Cave {
     let mut tiles = Vec::new();
     let mut units = HashMap::new();
     let mut unit_id = 0;
@@ -231,38 +405,41 @@ fn parse_input(input: Input) -> (HashMap<i32, RefCell<Unit>>, Map, i32) {
         }
     }
 
-    (
-        units,
+    Cave::new(
         Map {
             tiles,
             dimension: 32,
         },
+        units,
         unit_id - 1,
     )
 }
 
 #[aoc(15)]
 fn solve_1(input: Input) {
-    let (units, map, unit_max_id) = parse_input(input);
+    let cave = parse_input(input);
+    cave.print();
 
-    map.print_with_units(&units);
-
-    let mut result = false;
     let mut turn_counter = 0;
     loop {
         turn_counter += 1;
-        println!("Turn {}", turn_counter);
-
-        for id in 0..unit_max_id {
-            let mut unit = units[&id].borrow_mut();
-            result |= unit.turn(&map, &units);
-        }
+        println!("==========\nTurn {}\n==========", turn_counter);
 
         // wtf rust, where is my do-while?
-        if !result {
+        if !cave.turn() {
             break;
         }
+
+        cave.print();
     }
+
+    let health_sum = cave.unit_health_sum();
+    println!(
+        "{} * {} = {}",
+        turn_counter,
+        health_sum,
+        turn_counter * health_sum
+    );
 }
 
 #[aoc(1)]
